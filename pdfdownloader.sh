947927f0c926758e8abdd292846979483fd764bf
guidiamond <gu1k1ll3rbr@hotmail.com>
2019-02-07 10:34:40 -0200

    working pdfdownloader

--- pdfdownloader.sh
+++ pdfdownloader.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 echo "Digite o nome do arquivo (ext: PDF)"
-read Filename
+read filename
 login="guilhermetb1"
 pass="249010"
 echo "Cole as URLS (press ENTER para baixar)"
 read URL
 
-URLS[0] = "${URL}"
+filename_array=(${filename})
+url_array=(${URL})
 count=0
 while [ ! -z "$URL" ];do
+    echo "Digite o nome do arquivo (ext: PDF)"
+    read filename
+    echo "Cole as URLS (press ENTER para baixar)"
     read URL
-    URLS+=("${URL}")
+    url_array+=("${URL}")
+    filename_array+=(${filename})
     count=$((count + 1))
 done
-echo ${URLS}
-# curl -u "${login}:${pass}" -o "${HOME}/Documents/cdd/${Filename}.pdf" "${URL}"
+count=$((count - 1))
+for i in $(seq 0 1 ${count});
+do
+    # echo ${i}
+    # echo ${filename[${i}]}
+    # echo ${url_array[${i}]} 
+    curl -u "${login}:${pass}" -o "${HOME}/Documents/cdd/${filename_array[${i}]}.pdf" "${url_array[${i}]}"
+done
